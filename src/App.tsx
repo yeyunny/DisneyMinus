edfb32d5a3605aa919b271568cee9638dde82a95
yeyun <hallo.yunye@gmail.com>
2023-07-18 22:46:12 +0900

    [ADD] styled-component 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,20 @@
-function App() {
-  return <div>crazy dolphin</div>;
-}
+import { Route, Switch } from "react-router-dom";
+import Main from "./main/Main";
+import Detail from "./detail/Detail";
+import Nav from "./Nav/Nav";
+
+const App = () => {
+  return (
+    <Switch>
+      <Nav />
+      <Route path="/">
+        <Main />
+      </Route>
+      <Route path="/detail">
+        <Detail />
+      </Route>
+    </Switch>
+  );
+};
 
 export default App;
---
+++ src/Nav/Nav.tsx
@@ -0,0 +1,19 @@
+import { AiFillHome } from "react-icons/ai";
+
+function Nav() {
+  return (
+    <div>
+      <img src="/img/Banana.jpg" alt="merong"></img>
+      <p>
+        <AiFillHome /> 홈
+      </p>
+      <p>검색</p>
+      <p>관심 콘텐츠</p>
+      <p>오리지널</p>
+      <p>영화</p>
+      <p>시리즈</p>
+    </div>
+  );
+}
+
+export default Nav;
