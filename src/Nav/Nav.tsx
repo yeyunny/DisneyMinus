020af61e41cd6998772aacb44cc4268f0b78c420
yeyun <hallo.yunye@gmail.com>
2023-07-19 14:24:58 +0900

    [ADD] styled-components 사용해보기(wip)

--- src/Nav/Nav.tsx
+++ src/Nav/Nav.tsx
@@ -1,17 +1,44 @@
-import { AiFillHome } from "react-icons/ai";
+import { AiFillHome, AiOutlinePlus, AiFillStar } from "react-icons/ai";
+import { FiSearch } from "react-icons/fi";
+import { PiFilmReelFill, PiTelevisionFill } from "react-icons/pi";
+import styled from "styled-components";
+
+const Container = styled.div`
+  display: flex;
+`;
+
+const Img = styled.img`
+  width: 100px;
+  height: 100px;
+`;
 
 function Nav() {
   return (
     <div>
-      <img src="/img/Banana.jpg" alt="데이터를 불러오지 못했습니다"></img>
+      <Img src="/img/Banana.jpg" alt="데이터를 불러오지 못했습니다"></Img>
       <p>
         <AiFillHome /> 홈
       </p>
-      <p>검색</p>
-      <p>관심 콘텐츠</p>
-      <p>오리지널</p>
-      <p>영화</p>
-      <p>시리즈</p>
+      <p>
+        <FiSearch />
+        검색
+      </p>
+      <p>
+        <AiOutlinePlus />
+        관심 콘텐츠
+      </p>
+      <p>
+        <AiFillStar />
+        오리지널
+      </p>
+      <p>
+        <PiFilmReelFill />
+        영화
+      </p>
+      <p>
+        <PiTelevisionFill />
+        시리즈
+      </p>
     </div>
   );
 }
