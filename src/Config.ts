c4eba7586e9a90296f6643902a0dff51a3fa4d34
yeyun <hallo.yunye@gmail.com>
2023-07-22 13:22:21 +0900

    wip

--- src/Config.ts
+++ src/Config.ts
@@ -1,3 +1,5 @@
 export const API_URL = "https://api.themoviedb.org/3/";
 
 export const IMAGE_BASE_URL = "https://image.tmdb.org/t/p/";
+
+export const API_KEY = "c0c4fcb4ed942551a03f4da3c4f66b15";
--- src/main/Main.tsx
+++ src/main/Main.tsx
@@ -1,4 +1,23 @@
+import { useEffect, useState } from "react";
+import { API_URL, API_KEY } from "../Config";
+import { response } from "express";
+
 function Main() {
+  const [Movies, setMovies] = useState([]);
+  const [MainMovieImage, setMainMovieImage] = useState();
+
+  useEffect(() => {
+    const endpoint = `${API_URL}movie/popular?api_key=${API_KEY}&language=en-US&page=1`;
+
+    fetch(endpoint)
+      .then((response) => response.json())
+      .then((response) => {
+        console.log(response);
+        // setMovies([...response.results]);
+        setMainMovieImage(MainMovieImage || response.results[0]);
+      });
+  }, []);
+
   return (
     <div>
       <p>Main</p>
