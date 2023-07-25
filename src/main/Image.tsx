9918ae66c562161e2e3895e12398fcc5bbb6d201
yeyun <hallo.yunye@gmail.com>
2023-07-25 20:40:03 +0900

    [DELETE] 불필요한 css 삭제

--- src/main/Image.tsx
+++ src/main/Image.tsx
@@ -2,14 +2,6 @@ import styled from "styled-components";
 
 const ContainerDiv = styled.div``;
 
-// const MovieImg = styled.img``;
-
-// const MovieInfo = styled.p``;
-
-// export interface Props {
-//   url: string;
-// }
-
 function Image({ url }: { url: string }) {
   return (
     <ContainerDiv>
